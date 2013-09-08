ce94aa1fbc85ae7e0e7ea7220a01e3351871d262
arielnh56 <ayoung@AYOUNG-PC2.rf.local>
2013-09-08 15:32:24 -0700

    Updated readme with lib info
    I discovered the loop was alternating between 6ms and 18ms due to the keypad scan. Force loop to 25ms and moved keypad scan to end of loop.
    Tweaked acceleration maximums to handle longer cycle time.

--- freddalek/freddalek.ino
+++ freddalek/freddalek.ino
@@ -56,6 +56,7 @@ const int EEPROM_headDown  =  9;
 const int EEPROM_headLeft  = 10;
 const int EEPROM_headRight = 11;
 
+const uint8_t LOOPTIME = 25;
 // corresponding vars
 int8_t eyeMin, domeZero, headZeroH;  // ACE raw pos
 int8_t domeMax, domeMin, headLeft, headRight; // relative -64 to 63
@@ -77,11 +78,11 @@ const int MOVE_RIGHT=2;
 const int MOVE_LEFT=-2;
 int8_t eyeDir=MOVE_STOP;
 int8_t domeDir=MOVE_STOP;
-const int EYE_MINSPEED_UP=180;
-const int EYE_MINSPEED_DOWN=160;
+const int EYE_MINSPEED_UP=170;
+const int EYE_MINSPEED_DOWN=150;
 int EYE_MAXSPEED=255;
-const int eyeUpAccel=4;
-const int eyeDownAccel=3;
+const int eyeUpAccel=20;
+const int eyeDownAccel=15;
 const int DOME_MINSPEED=160;
 int DOME_MAXSPEED=255;
 const int domeAccel=1;
@@ -124,6 +125,7 @@ uint8_t eyeTarget;
 uint32_t eyeLastChange;
 int8_t domeTarget;
 uint32_t domeLastChange;
+uint32_t lastLoop;
 
 void setup() {
   TCCR1B = TCCR1B & 0b11111000 | 0x01;
@@ -253,16 +255,206 @@ void setup() {
   digitalWrite(LED_HEAD_RED, HIGH);
   digitalWrite(LED_HEAD_YELLOW, HIGH);
   digitalWrite(LED_HEAD_GREEN, HIGH);
+
+  lastLoop = millis();
 }
 
+
+
 void loop() {
-   delay(1);
+   char key = keypad.getKey();
+   if (lastLoop + LOOPTIME > millis()) {
+     delay(lastLoop + LOOPTIME - millis());
+   }
+   lastLoop = millis();
     
   chuck.update();
   int chuckPitch = chuck.readPitch10();
-  char key = keypad.getKey();
-     
+  eyePos = eyeACE.upos();
+  domePos = domeACE.pos();
+  headPos = headACE.pos();
+
+  if (chuckPitch > headZeroV*10 + 15) {
+    digitalWrite(LED_HEAD_RED, LOW);
+  } else {
+    digitalWrite(LED_HEAD_RED, HIGH);
+  }
+  if (abs(chuckPitch - headZeroV*10) < 30) {
+    digitalWrite(LED_HEAD_YELLOW, LOW);
+  } else {
+    digitalWrite(LED_HEAD_YELLOW, HIGH);
+  }
+  if (chuckPitch < headZeroV*10 - 15) {
+    digitalWrite(LED_HEAD_GREEN, LOW);
+  } else {
+    digitalWrite(LED_HEAD_GREEN, HIGH);
+  }
+   // motion decisions
+  switch (mode) {
+    case MODE_FOLLOW:
+      switch (mode_2) {
+        case MODE_FOLLOW_NORMAL:
+          tmp_16 = chuckPitch - headZeroV*10;
+          if (abs(tmp_16) < 30) {
+            eyeTarget = eyeZero;
+          } else if (tmp_16 > 0) { // up
+            eyeTarget = constrain(eyeZero+(tmp_16-30)/10,eyeZero,eyeMax);            
+          } else { // down
+            eyeTarget = constrain(eyeZero+(tmp_16+30)/10,0,eyeZero);            
+          }
+          domeTarget = headPos;
+          break;
+ 
+       case MODE_FOLLOW_LEVEL:
+          eyeTarget = eyeZero;
+          domeTarget = headPos;
+          break;
+      }
+      break;
+    case MODE_ZERO:
+       if (mode_2 == MODE_ZERO_WAIT) {
+         eyeTarget = eyePos;
+         domeTarget = domePos;
+         eyeDir = MOVE_STOP;
+         domeDir = MOVE_STOP;
+       }
+       break;
+    case MODE_AUTO:
+      if (mode_2 == MODE_AUTO_SCAN) {
+        if (mode_3 == MODE_AUTO_SCAN_RIGHT) {
+          domeTarget = domeMax;
+          if (domePos > 32) {
+            mode_3 = MODE_AUTO_SCAN_LEFT;
+          } 
+        } else  if (mode_3 == MODE_AUTO_SCAN_LEFT) {
+          domeTarget = domeMin;
+          if (domePos < -32) {
+            mode_3 = MODE_AUTO_SCAN_RIGHT;
+          } 
+        } 
+      }
+      break;
+  }  
+#ifdef DEBUG
+//  domePos = domeACE.pos();
+//  Serial.print(" domeRawPos ");
+//  Serial.print(domeACE.rawPos());
+//  Serial.print(" eyeRawPos ");
+//  Serial.print(eyeACE.rawPos());
+//  Serial.print("domePos ");
+//  Serial.print(domePos);
+//  Serial.print(" domeTarget ");
+//  Serial.print(domeTarget);
+//  Serial.print(" domeDir ");
+//  Serial.print(domeDir);
+//  Serial.print(" domeSpeed ");
+//  Serial.print(domeSpeed);
+//  Serial.print(" targetDomeSpeed ");
+//  Serial.println(targetDomeSpeed);
+Serial.print(millis());
+Serial.print(" ");
+Serial.print(micros());
+Serial.print(" ");
+Serial.print(" eyePos ");
+Serial.print(eyePos);
+Serial.print(" eyeTarget ");
+Serial.print(eyeTarget);
+Serial.print(" eyeDir ");
+Serial.print(eyeDir);
+Serial.print(" eyeSpeed ");
+Serial.print(eyeSpeed);
+Serial.print(" targetEyeSpeed ");
+Serial.print(targetEyeSpeed);
+Serial.print(" chuckPitch ");
+Serial.println(chuckPitch);
+#endif
+
+  // motor control
+  // target speed same dir
+  // same dir?
+  if (((eyeTarget > eyePos) && eyeDir == MOVE_UP)) {
+    targetEyeSpeed = constrain(EYE_MINSPEED_UP-5+(abs(eyeTarget - eyePos)*5),EYE_MINSPEED_UP,EYE_MAXSPEED);
+    if (eyeSpeed < targetEyeSpeed) { // speed up
+      eyeSpeed=constrain(eyeSpeed + eyeUpAccel,EYE_MINSPEED_UP,EYE_MAXSPEED);
+    } else if (eyeSpeed > targetEyeSpeed) { // slow down
+      eyeSpeed=constrain(eyeSpeed - eyeDownAccel,EYE_MINSPEED_UP,EYE_MAXSPEED);
+    }
+  } else if ((eyeTarget < eyePos) && eyeDir == MOVE_DOWN) {
+    targetEyeSpeed = constrain(EYE_MINSPEED_DOWN-5+(abs(eyeTarget - eyePos)*5),EYE_MINSPEED_DOWN,EYE_MAXSPEED);
+    if (eyeSpeed < targetEyeSpeed) { // speed up
+      eyeSpeed=constrain(eyeSpeed + eyeDownAccel,EYE_MINSPEED_UP,EYE_MAXSPEED);
+    } else if (eyeSpeed > targetEyeSpeed) { // slow down
+      eyeSpeed=constrain(eyeSpeed - eyeUpAccel,EYE_MINSPEED_UP,EYE_MAXSPEED);
+    }
+  } else if ((eyeTarget > eyePos) && eyeDir == MOVE_DOWN) { // direction change
+    if (eyeSpeed > EYE_MINSPEED_DOWN) eyeSpeed=constrain(eyeSpeed - eyeUpAccel,EYE_MINSPEED_DOWN,EYE_MAXSPEED); // slow down
+    if (eyeSpeed <= EYE_MINSPEED_DOWN) eyeDir = MOVE_UP;
+  } else if ((eyeTarget < eyePos) && eyeDir == MOVE_UP) { // direction change
+    if (eyeSpeed > EYE_MINSPEED_UP) eyeSpeed=constrain(eyeSpeed - eyeDownAccel,EYE_MINSPEED_UP,EYE_MAXSPEED); // slow down
+    if (eyeSpeed <= EYE_MINSPEED_UP) eyeDir = MOVE_DOWN;
+  } else if (eyeTarget==eyePos) { // stop
+    eyeDir = MOVE_STOP;
+    eyeSpeed=0;
+  } else if (eyePos < eyeTarget+2 && eyeDir == MOVE_STOP) {
+    eyeDir = MOVE_UP;
+    eyeSpeed=EYE_MINSPEED_UP;
+  } else if (eyePos > eyeTarget-2 && eyeDir == MOVE_STOP) {
+    eyeDir = MOVE_DOWN;
+    eyeSpeed=EYE_MINSPEED_DOWN;
+  }
+
+  // same dir?
+  if (((domeTarget < domePos) && domeDir == MOVE_LEFT) || ((domeTarget > domePos) && domeDir == MOVE_RIGHT)) {
+    targetDomeSpeed = constrain(DOME_MINSPEED-3+(abs(domeTarget - domePos)*3),DOME_MINSPEED,DOME_MAXSPEED);
+    if (domeSpeed < targetDomeSpeed) { // speed up
+      domeSpeed=constrain(domeSpeed + domeAccel,DOME_MINSPEED,DOME_MAXSPEED);
+    } else if (domeSpeed > targetDomeSpeed) { // slow down
+      domeSpeed=constrain(domeSpeed - domeDecel,DOME_MINSPEED,DOME_MAXSPEED);
+    }
+  } else if (((domeTarget > domePos) && domeDir == MOVE_LEFT) || ((domeTarget < domePos) && domeDir == MOVE_RIGHT)) {
+    if (domeSpeed > DOME_MINSPEED) domeSpeed=constrain(domeSpeed - domeDecel,DOME_MINSPEED,DOME_MAXSPEED); // slow down
+    if (domeSpeed <= DOME_MINSPEED) domeDir *= -1;
+  } else if (domeTarget==domePos) { // stop
+    domeDir = MOVE_STOP;
+    domeSpeed=0;
+  } else if (domePos < domeTarget && domeDir == MOVE_STOP) {
+    domeDir = MOVE_RIGHT;
+    domeSpeed=DOME_MINSPEED;
+  } else if (domePos > domeTarget && domeDir == MOVE_STOP) {
+    domeDir = MOVE_LEFT;
+    domeSpeed=DOME_MINSPEED;
+  }
+
+
+  if (domeDir == MOVE_STOP) { // brake
+    digitalWrite(domeM1,LOW);
+    digitalWrite(domeM2,LOW);
+    analogWrite(domePWM,0);
+  } else if (domeDir == MOVE_LEFT) {
+    digitalWrite(domeM1,LOW);
+    digitalWrite(domeM2,HIGH);
+    analogWrite(domePWM,domeSpeed);
+  } else if (domeDir == MOVE_RIGHT) {
+    digitalWrite(domeM1,HIGH);
+    digitalWrite(domeM2,LOW);
+    analogWrite(domePWM,domeSpeed);
+  }
+
+  if (eyeDir == MOVE_STOP) { // brake
+    digitalWrite(eyeM1,LOW);
+    digitalWrite(eyeM2,LOW);
+    analogWrite(eyePWM,0);
+  } else if (eyeDir == MOVE_UP) {
+    digitalWrite(eyeM1,HIGH);
+    digitalWrite(eyeM2,LOW);
+    analogWrite(eyePWM,eyeSpeed);
+  } else if (eyeDir == MOVE_DOWN) {
+    digitalWrite(eyeM1,LOW);
+    digitalWrite(eyeM2,HIGH);
+    analogWrite(eyePWM,eyeSpeed);
+  }
   if (key){
+    
 #ifdef DEBUG
     Serial.print("key press ");
     Serial.println(key);
@@ -303,13 +495,14 @@ void loop() {
             } // switch key
             break;          
           case MODE_ZERO_SET_HEAD:
+            chuck.update();
             switch (key) {
               case '2': // top limit
-                headUp = chuckPitch/10;
+                headUp = round(chuck.readPitch10()/10);
                 EEPROM.write(EEPROM_headUp,headUp);
                 break;
               case '8': // bottom limit
-                headDown = chuckPitch/10;
+                headDown = round(chuck.readPitch10()/10);
                 EEPROM.write(EEPROM_headDown,headDown);
                 break;
               case '4': // left limit
@@ -317,7 +510,7 @@ void loop() {
               case '6': // right limit
                 break;
               case '5': // center
-                headZeroV = chuckPitch/10;
+                headZeroV = round(chuck.readPitch10()/10);
                 EEPROM.write(EEPROM_headZeroV,headZeroV);
                 headZeroH = headACE.rawPos();
                 headACE.setZero(headZeroH);
@@ -466,185 +659,6 @@ void loop() {
         break;
     } // switch mode
   } // key processing
-
-  eyePos = eyeACE.upos();
-  domePos = domeACE.pos();
-  headPos = headACE.pos();
-  if (chuckPitch > headZeroV*10 + 15) {
-    digitalWrite(LED_HEAD_RED, LOW);
-  } else {
-    digitalWrite(LED_HEAD_RED, HIGH);
-  }
-  if (abs(chuckPitch - headZeroV*10) < 30) {
-    digitalWrite(LED_HEAD_YELLOW, LOW);
-  } else {
-    digitalWrite(LED_HEAD_YELLOW, HIGH);
-  }
-  if (chuckPitch < headZeroV*10 - 15) {
-    digitalWrite(LED_HEAD_GREEN, LOW);
-  } else {
-    digitalWrite(LED_HEAD_GREEN, HIGH);
-  }
-   // motion decisions
-  switch (mode) {
-    case MODE_FOLLOW:
-      switch (mode_2) {
-        case MODE_FOLLOW_NORMAL:
-          tmp_16 = chuckPitch - headZeroV*10;
-          if (abs(tmp_16) < 30) {
-            eyeTarget = eyeZero;
-          } else if (tmp_16 > 0) { // up
-            eyeTarget = constrain(eyeZero+(tmp_16-30)/10,eyeZero,eyeMax);            
-          } else { // down
-            eyeTarget = constrain(eyeZero+(tmp_16+30)/10,0,eyeZero);            
-          }
-          domeTarget = headPos;
-          break;
- 
-       case MODE_FOLLOW_LEVEL:
-          eyeTarget = eyeZero;
-          domeTarget = headPos;
-          break;
-      }
-      break;
-    case MODE_ZERO:
-       if (mode_2 == MODE_ZERO_WAIT) {
-         eyeTarget = eyePos;
-         domeTarget = domePos;
-         eyeDir = MOVE_STOP;
-         domeDir = MOVE_STOP;
-       }
-       break;
-    case MODE_AUTO:
-      if (mode_2 == MODE_AUTO_SCAN) {
-        if (mode_3 == MODE_AUTO_SCAN_RIGHT) {
-          domeTarget = domeMax;
-          if (domePos > 32) {
-            mode_3 = MODE_AUTO_SCAN_LEFT;
-          } 
-        } else  if (mode_3 == MODE_AUTO_SCAN_LEFT) {
-          domeTarget = domeMin;
-          if (domePos < -32) {
-            mode_3 = MODE_AUTO_SCAN_RIGHT;
-          } 
-        } 
-      }
-      break;
-  }  
-#ifdef DEBUG
-//  domePos = domeACE.pos();
-//  Serial.print(" domeRawPos ");
-//  Serial.print(domeACE.rawPos());
-//  Serial.print(" eyeRawPos ");
-//  Serial.print(eyeACE.rawPos());
-//  Serial.print("domePos ");
-//  Serial.print(domePos);
-//  Serial.print(" domeTarget ");
-//  Serial.print(domeTarget);
-//  Serial.print(" domeDir ");
-//  Serial.print(domeDir);
-//  Serial.print(" domeSpeed ");
-//  Serial.print(domeSpeed);
-//  Serial.print(" targetDomeSpeed ");
-//  Serial.println(targetDomeSpeed);
-Serial.print(" eyePos ");
-Serial.print(eyePos);
-Serial.print(" eyeTarget ");
-Serial.print(eyeTarget);
-Serial.print(" eyeDir ");
-Serial.print(eyeDir);
-Serial.print(" eyeSpeed ");
-Serial.print(eyeSpeed);
-Serial.print(" targetEyeSpeed ");
-Serial.println(targetEyeSpeed);
-Serial.print(" chuckPitch ");
-Serial.println(chuckPitch);
-#endif
-
-  // motor control
-  // target speed same dir
-  // same dir?
-  if (((eyeTarget > eyePos) && eyeDir == MOVE_UP)) {
-    targetEyeSpeed = constrain(EYE_MINSPEED_UP-5+(abs(eyeTarget - eyePos)*5),EYE_MINSPEED_UP,EYE_MAXSPEED);
-    if (eyeSpeed < targetEyeSpeed) { // speed up
-      eyeSpeed=constrain(eyeSpeed + eyeUpAccel,EYE_MINSPEED_UP,EYE_MAXSPEED);
-    } else if (eyeSpeed > targetEyeSpeed) { // slow down
-      eyeSpeed=constrain(eyeSpeed - eyeDownAccel,EYE_MINSPEED_UP,EYE_MAXSPEED);
-    }
-  } else if ((eyeTarget < eyePos) && eyeDir == MOVE_DOWN) {
-    targetEyeSpeed = constrain(EYE_MINSPEED_DOWN-5+(abs(eyeTarget - eyePos)*5),EYE_MINSPEED_DOWN,EYE_MAXSPEED);
-    if (eyeSpeed < targetEyeSpeed) { // speed up
-      eyeSpeed=constrain(eyeSpeed + eyeDownAccel,EYE_MINSPEED_UP,EYE_MAXSPEED);
-    } else if (eyeSpeed > targetEyeSpeed) { // slow down
-      eyeSpeed=constrain(eyeSpeed - eyeUpAccel,EYE_MINSPEED_UP,EYE_MAXSPEED);
-    }
-  } else if ((eyeTarget > eyePos) && eyeDir == MOVE_DOWN) { // direction change
-    if (eyeSpeed > EYE_MINSPEED_DOWN) eyeSpeed=constrain(eyeSpeed - eyeUpAccel,EYE_MINSPEED_DOWN,EYE_MAXSPEED); // slow down
-    if (eyeSpeed <= EYE_MINSPEED_DOWN) eyeDir = MOVE_UP;
-  } else if ((eyeTarget < eyePos) && eyeDir == MOVE_UP) { // direction change
-    if (eyeSpeed > EYE_MINSPEED_UP) eyeSpeed=constrain(eyeSpeed - eyeDownAccel,EYE_MINSPEED_UP,EYE_MAXSPEED); // slow down
-    if (eyeSpeed <= EYE_MINSPEED_UP) eyeDir = MOVE_DOWN;
-  } else if (eyeTarget==eyePos) { // stop
-    eyeDir = MOVE_STOP;
-    eyeSpeed=0;
-  } else if (eyePos < eyeTarget+2 && eyeDir == MOVE_STOP) {
-    eyeDir = MOVE_UP;
-    eyeSpeed=EYE_MINSPEED_UP;
-  } else if (eyePos > eyeTarget-2 && eyeDir == MOVE_STOP) {
-    eyeDir = MOVE_DOWN;
-    eyeSpeed=EYE_MINSPEED_DOWN;
-  }
-
-  // same dir?
-  if (((domeTarget < domePos) && domeDir == MOVE_LEFT) || ((domeTarget > domePos) && domeDir == MOVE_RIGHT)) {
-    targetDomeSpeed = constrain(DOME_MINSPEED-3+(abs(domeTarget - domePos)*3),DOME_MINSPEED,DOME_MAXSPEED);
-    if (domeSpeed < targetDomeSpeed) { // speed up
-      domeSpeed=constrain(domeSpeed + domeAccel,DOME_MINSPEED,DOME_MAXSPEED);
-    } else if (domeSpeed > targetDomeSpeed) { // slow down
-      domeSpeed=constrain(domeSpeed - domeDecel,DOME_MINSPEED,DOME_MAXSPEED);
-    }
-  } else if (((domeTarget > domePos) && domeDir == MOVE_LEFT) || ((domeTarget < domePos) && domeDir == MOVE_RIGHT)) {
-    if (domeSpeed > DOME_MINSPEED) domeSpeed=constrain(domeSpeed - domeDecel,DOME_MINSPEED,DOME_MAXSPEED); // slow down
-    if (domeSpeed <= DOME_MINSPEED) domeDir *= -1;
-  } else if (domeTarget==domePos) { // stop
-    domeDir = MOVE_STOP;
-    domeSpeed=0;
-  } else if (domePos < domeTarget && domeDir == MOVE_STOP) {
-    domeDir = MOVE_RIGHT;
-    domeSpeed=DOME_MINSPEED;
-  } else if (domePos > domeTarget && domeDir == MOVE_STOP) {
-    domeDir = MOVE_LEFT;
-    domeSpeed=DOME_MINSPEED;
-  }
-
-
-  if (domeDir == MOVE_STOP) { // brake
-    digitalWrite(domeM1,LOW);
-    digitalWrite(domeM2,LOW);
-    analogWrite(domePWM,0);
-  } else if (domeDir == MOVE_LEFT) {
-    digitalWrite(domeM1,LOW);
-    digitalWrite(domeM2,HIGH);
-    analogWrite(domePWM,domeSpeed);
-  } else if (domeDir == MOVE_RIGHT) {
-    digitalWrite(domeM1,HIGH);
-    digitalWrite(domeM2,LOW);
-    analogWrite(domePWM,domeSpeed);
-  }
-
-  if (eyeDir == MOVE_STOP) { // brake
-    digitalWrite(eyeM1,LOW);
-    digitalWrite(eyeM2,LOW);
-    analogWrite(eyePWM,0);
-  } else if (eyeDir == MOVE_UP) {
-    digitalWrite(eyeM1,HIGH);
-    digitalWrite(eyeM2,LOW);
-    analogWrite(eyePWM,eyeSpeed);
-  } else if (eyeDir == MOVE_DOWN) {
-    digitalWrite(eyeM1,LOW);
-    digitalWrite(eyeM2,HIGH);
-    analogWrite(eyePWM,eyeSpeed);
-  }
 
 }
 
